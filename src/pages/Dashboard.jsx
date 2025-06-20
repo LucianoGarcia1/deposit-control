48a9adf21fd20117459b559f05fc5aa815a6e60c
LucianoGarcia1 <lucianogarcia0210@gmail.com>
2025-06-19 21:23:00 -0300

    style: Melhorias no design

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,11 +5,14 @@ import { Modal } from "../components/Modal/Modal";
 import { IoAddOutline } from "react-icons/io5";
 import { ButtonModal } from "../components/Button/Button_Modal";
 import { Title } from "../components/Title/Title";
+import { Deposits } from "../components/Deposits/Deposits";
+import Ilustration from "../../public/deposit.svg";
 
 function Dashboard() {
   const [activeMenu, setActiveMenu] = useState(false);
   const [activeModal, setActiveModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [depositSelected, setDepositSelected] = useState(null);
 
   const handleMenu = () => {
     setActiveMenu((prev) => !prev);
@@ -19,18 +22,29 @@ function Dashboard() {
     setActiveModal((prev) => !prev);
   };
 
+  const handleEdit = (deposito) => {
+    setDepositSelected(deposito);
+    setActiveModal(true);
+  };
+
   return (
     <section
       className={`w-full h-screen relative transition-all duration-300 bg-white ${
         activeMenu ? "lg:pl-[300px]" : "pl-0"
       }`}
     >
-      <main className="w-full min-h-screen bg-white p-4 flex flex-col gap-8">
+      <main className="w-full min-h-screen bg-white py-4 sm:p-4 flex flex-col gap-8">
         <Header active={activeMenu} handleMenu={handleMenu} />
         <div className="w-full px-4">
-          <Title>Gerenciador de Depósitos</Title>
+          <Title mxWidth="max-w-[400px]">Gerenciador de Depósitos</Title>
 
-          <div className="w-full mt-8"></div>
+          <div className="w-full mt-6 flex gap-10 items-start flex-col-reverse md:flex-row relative">
+            <Deposits handleEdit={handleEdit} />
+
+            <div className="max-w-[550px] xl:fixed xl:right-24">
+              <img src={Ilustration} alt="Deposits Ilustration" />
+            </div>
+          </div>
 
           <ButtonModal
             title="Criar Depósito"
@@ -45,6 +59,7 @@ function Dashboard() {
               handleClose={handleModal}
               setIsLoading={setIsLoading}
               isLoading={isLoading}
+              depositSelected={depositSelected}
             />
           )}
         </div>
